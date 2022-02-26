ac808f1b1af24bc5781dca5db0d084c14982d5e0
Clesioks <kornalewski@outlook.com>
2022-02-26 16:58:09 -0300

    conn mongo

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 import express from "express";
 import notes from "../backend/data/notes.js";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 
 const app = express();
 dotenv.config();
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("Olá Mundo!");
