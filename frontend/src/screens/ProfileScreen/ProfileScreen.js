e491e67f675e61fca9c4bac068dc057832a7885c
Clesioks <kornalewski@outlook.com>
2022-03-24 17:13:26 -0300

    update form

--- frontend/src/screens/ProfileScreen/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen/ProfileScreen.js
@@ -1,7 +1,12 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Col, Row, Button, Form } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { updateProfile } from "../../actions/userActions";
+import ErrorMessage from "../../components/ErrorMessage";
 import MainScreen from "../../components/MainScreen";
+import Loading from "../../components/Loading";
+import "./ProfileScreen.css";
 
 const ProfileScreen = () => {
   const [name, setName] = useState("");
@@ -19,11 +24,116 @@ const ProfileScreen = () => {
   const userUpdate = useSelector((state) => state.userUpdate);
   const { loading, error, success } = userUpdate;
 
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!userInfo) {
+      navigate("/");
+    } else {
+      setName(userInfo.name);
+      setEmail(userInfo.email);
+      setPic(userInfo.pic);
+    }
+  }, [navigate, userInfo]);
+
+  const postDetails = (pics) => {
+    if (!pics) {
+      return setPicMessage("Please select an Image");
+    }
+    setPicMessage(null);
+    if (pics.type === "image/jpeg" || pics.type === "image/png") {
+      const data = new FormData();
+      data.append("file", pics);
+      data.append("upload_preset", "notezipper");
+      data.append("cloud_name", "dgrlzoyzt");
+      fetch("https://api.cloudinary.com/v1_1/dgrlzoyzt/image/upload", {
+        method: "post",
+        body: data,
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          setPic(data.url.toString());
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } else {
+      return setPicMessage("Please Select an Image");
+    }
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    if (password === confirmPassword)
+      dispatch(updateProfile({ name, email, password, pic }));
+  };
+
   return (
     <MainScreen title="EDIT PROFILE">
       <div>
         <Row className="profileContainer">
-          <Col md={6}>Form</Col>
+          <Col md={6}>
+            <Form onSubmit={submitHandler}>
+              {loading && <Loading />}
+              {success && (
+                <ErrorMessage variant="success">
+                  Updated Successfully
+                </ErrorMessage>
+              )}
+              {error && <ErrorMessage variant="danger">{error}</ErrorMessage>}
+              <Form.Group controlId="name">
+                <Form.Label>Name</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter Name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                ></Form.Control>
+              </Form.Group>
+              <Form.Group controlId="email">
+                <Form.Label>Email Address</Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="Enter Email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                ></Form.Control>
+              </Form.Group>
+              <Form.Group controlId="password">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Enter Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                ></Form.Control>
+              </Form.Group>
+              <Form.Group controlId="confirmPassword">
+                <Form.Label>Confirm Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Confirm Password"
+                  value={confirmPassword}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                ></Form.Control>
+              </Form.Group>{" "}
+              {picMessage && (
+                <ErrorMessage variant="danger">{picMessage}</ErrorMessage>
+              )}
+              <Form.Group controlId="formFile" className="mb-3">
+                <Form.Label>Upload Profile Picture</Form.Label>
+                <Form.Control
+                  type="file"
+                  onChange={(e) => postDetails(e.target.files[0])}
+                />
+              </Form.Group>
+              <Button type="submit" varient="primary">
+                Update
+              </Button>
+            </Form>
+          </Col>
           <Col
             style={{
               display: "flex",
