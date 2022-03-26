05c872c7e99cdac06f61625b48d1f349dd73f154
Clesioks <kornalewski@outlook.com>
2022-03-26 12:55:03 -0300

    end

--- backend/server.js
+++ backend/server.js
@@ -4,22 +4,36 @@ import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
 import noteRoutes from "./routes/noteRoutes.js";
 import { errorHandler, notFound } from "./middlewares/errorMiddlewares.js";
+import path from "path";
 
 const app = express();
 dotenv.config();
 connectDB();
 app.use(express.json());
 
-// app.get("/", (req, res) => {
-//   res.send("Olá Mundo!");
-// });
-
-// app.get("/api/notes", (req, res) => {
-//   res.json(notes);
-// });
-
 app.use("/api/users", userRoutes);
 app.use("/api/notes", noteRoutes);
+
+// ---------------- deployment ----------------------
+
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(
+      path.resolve(path.resolve(__dirname, "frontend", "build", "index.html"))
+    );
+  });
+} else {
+  app.get("/"),
+    (req, res) => {
+      res.send("PI is running");
+    };
+}
+
+// ---------------- deployment ----------------------
 
 app.use(notFound);
 app.use(errorHandler);
